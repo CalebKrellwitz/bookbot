63046fe3aa358897ae49294f9ad344008b62979a
CalebKrellwitz <cwkrellwitz@gmail.com>
2025-06-04 09:30:13 -0400

    implement book filepath as an argument rather than hard coding

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import stats
+import sys
 
 def get_book_text(book_filepath):
     with open(book_filepath) as book:
         return book.read()
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     word_count = stats.get_word_count(book_text)
     character_counts = stats.get_character_counts(book_text)
     sorted_character_counts = stats.sort_character_counts(character_counts)
 
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {word_count} total words\n--------- Character Count -------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------\nFound {word_count} total words\n--------- Character Count -------")
 
     for character in sorted_character_counts:
         if character["char"].isalpha():
